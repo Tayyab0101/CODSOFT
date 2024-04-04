4e84706702cbbd0c56bbbdbb49f18a88decefc34
Tayyab0101 <tayyabusman7860@gmail.com>
2024-04-04 16:59:06 +0500

    minor changes

--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,23 @@
+# Funcs
 def add(a, b):
     return a + b
-
 def subtract(a, b):
     return a - b
-
 def multiply(a, b):
     return a * b
-
 def divide(a, b):
     if b == 0:
-        return "Error! Division by zero."
-    elif a % b == 0:  # Check if the result is an integer
+        return "Cannot Divide by Zero."
+    elif a % b == 0:
         return int(a / b)
     else:
         return a / b
 
 def calculator():
-    print("Welcome to Simple Calculator")
-    print("Select operation:")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-
-    choice = input("Enter choice (1/2/3/4): ")
+    print("Calculator!!!")
+    print("Sel Op:1 for add, 2 for Subtract, 3 for Multiply and 4 for Division")
+    
+    choice = input("Enter operation: ")
 
     a = float(input("Enter first number: "))
     b = float(input("Enter second number: "))
