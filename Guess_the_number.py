698032827fa3b2731e5d06e9b453a48ab73d4746
Tayyab0101 <tayyabusman7860@gmail.com>
2024-04-27 19:20:31 +0500

    chnages

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,12 +1,14 @@
 import random
 
-print(''' 
-    _     _  _  _   __   _              _  _  _ 
+print('''
+ 
+    _     _  _  _   __   _              _  _  _
    / `/ //_`/_`/_`  //_//_`  /|// //|,//_)/_`/_/
-  /_;/_//_,._/._/  // //_,  / |/_//  //_)/_,/ \ 
-                                               
+  /_;/_//_,._/._/  // //_,  / |/_//  //_)/_,/ \_
+
+
+Let me think of a number between 1 - 50.
 ''')
-print("Let me think of a number between 1 - 50.")
 guess1 = random.randint(1, 50)
 
 level = input("Choose your difficulty level (Hard or Easy): ").lower()
@@ -16,8 +18,18 @@ def guess():
     print(f"You have only {attempts} attempts to guess.")
     
     for i in range(1, attempts + 1):
-        guess = int(input("Make a guess: "))
-        diff = guess - guess1
+        while True:
+            guess = input("Make a guess: ")
+            if guess.isdigit():
+                guess = int(guess)
+                if 1 <= guess <= 50:
+                    break
+                else:
+                    print("Guess out of range. Please enter a number between 1 and 50.")
+            else:
+                print("Invalid input. Please enter a number.")
+        
+        diff = abs(guess - guess1)
         
         if guess == guess1:
             print("Correct. You won")
@@ -27,8 +39,8 @@ def guess():
         elif diff > 10:
             print("Far")
         elif diff > 5:
-            print("Somewhat Close.")
-        else:
+            print("Somewhat Far.")
+        elif diff > 0:
             print("Almost there.")
         
         print(f"You are left with {attempts - i} guesses.")
