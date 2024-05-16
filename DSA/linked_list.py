bbd19101b273c6a9f437f6dd01d9be9bc42dc61a
Tayyab0101 <tayyabusman7860@gmail.com>
2024-05-17 00:57:02 +0500

    ll changes

--- DSA/linked_list.py
+++ DSA/linked_list.py
@@ -2,18 +2,19 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
 class LinkedList:
     def __init__(self):
         self.head = None
         
-    def insert_at_begining(self, data):
+    def insert_at_beginning(self, data):
         new_node = Node(data) 
-        new_node.next = self.head # ref to next is self.head = None because we have only single Node by now
-        self.head = new_node # when we say new node that means its adress or memory location is at head now
+        new_node.next = self.head
+        self.head = new_node
         
     def insert_at_end(self, data):
         if self.head is None:
-            self.insert_at_begining(data)
+            self.insert_at_beginning(data)
             return
 
         itr = self.head
@@ -29,19 +30,43 @@ class LinkedList:
         count = 0 
         itr = self.head
         while itr:
-            count +=1
+            count += 1
             itr = itr.next
         return count    
     
     def remove_at(self, index):
-        if 0>=index or index>=self.get_length():
+        if index < 0 or index >= self.get_length():
             raise Exception("Invalid Index")
         
+        if index == 0:
+            self.head = self.head.next
+            return
+
         count = 0
         itr = self.head
         while itr:
-            if count == index -1:
+            if count == index - 1:
                 itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+        
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                new_node = Node(data)
+                new_node.next = itr.next
+                itr.next = new_node
+                break
             itr = itr.next
             count += 1
         
@@ -57,10 +82,11 @@ class LinkedList:
         print("None")
             
 new_list = LinkedList()
-new_list.insert_at_begining(16)
+new_list.insert_at_beginning(16)
 new_list.insert_at_end(45)
-new_list.insert_at_begining(75)
+new_list.insert_at_beginning(75)
 new_list.insert_value(["Mango", "Orange"])
 new_list.remove_at(4)
+new_list.insert_at(1, 26)
 new_list.print_list()
 print(f"length: {new_list.get_length()}")
