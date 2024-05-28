df4f569ba6e60592034dd92048c9dd00affdb13c
Tayyab0101 <tayyabusman7860@gmail.com>
2024-05-28 08:21:57 +0500

    DsA

---
+++ DSA/queue.py
@@ -0,0 +1,51 @@
+# # we can aslo do it using deque from collections as 
+# from collections import deque  
+# class Queue:
+#     def __init__(self):
+#         self.queue = deque()
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def enqueue(self, item):
+        self.queue.append(item)
+    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            return "Its an empty queue"
+    
+    def front(self):
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            return "its an empty queue"
+        
+    def rear(self):
+        if not self.is_empty():
+            return self.queue[-1]
+        else:
+            return "Its an empty queue"
+        
+    def display(self):
+        return self.queue
+    
+new_queue = Queue()
+print(new_queue.is_empty())
+
+new_queue.enqueue(5)
+new_queue.enqueue(6)
+new_queue.enqueue(7)
+new_queue.enqueue(8)
+print(new_queue.display())
+
+new_queue.dequeue()
+print(new_queue.display())
+
+print(new_queue.front())
+print(new_queue.rear())
